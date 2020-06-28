848337f91c5594d95a958637b1285d0a99766446
ilyastrijakov <pasv.qa.student5@gmail.com>
2020-06-28 16:25:29 -0400

    june 28 update

--- wdio.conf.js
+++ wdio.conf.js
@@ -18,7 +18,7 @@ exports.config = {
     //
     specs: [
         './test/elements.js',
-        './test/defaultFunctionality.js'
+        //'./test/defaultFunctionality.js'
     ],
     // Patterns to exclude.
     exclude: [
@@ -66,7 +66,7 @@ exports.config = {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'info',
+    logLevel: 'silent',
     //
     // Set specific log levels per logger
     // loggers:
